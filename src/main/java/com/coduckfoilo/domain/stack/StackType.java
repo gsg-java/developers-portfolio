587452b1b8ec85e8a61b2c3548d396ec5217ae52
YONGGEUN KWON <kingbbode@gmail.com>
2018-02-03 21:30:34 +0900

    default settings.

--- src/main/java/com/coduckfoilo/domain/stack/StackType.java
+++ src/main/java/com/coduckfoilo/domain/stack/StackType.java
@@ -4,4 +4,10 @@ package com.coduckfoilo.domain.stack;
  * Created by YG-MAC on 2018. 1. 21..
  */
 public enum StackType {
+    LANGUAGE,
+    OS,
+    BACKEND,
+    FRONTEND,
+    DATABASE,
+    TOOL
 }
